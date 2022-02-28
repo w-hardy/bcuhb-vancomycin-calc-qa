2f1377a0888482db04c0b70afbc8e03a68586bd4
w-hardy <51114571+w-hardy@users.noreply.github.com>
2022-02-28 15:06:52 +0000

    Added random seed and reordered columns

--- r_scripts/popn_sim.R
+++ r_scripts/popn_sim.R
@@ -2,12 +2,16 @@
 library(tidyverse)
 
 #### Data simulation ####
-n_sims <- 10
+set.seed(20220217)
+n_sims <- 1000
 sex <- round(runif(n_sims, min = 0, max = 1))
 sex <- factor(sex, levels = 0:1, labels = c("Female", "Male"))
-age <- runif(n_sims, min = 18, max = 140)
-height <- runif(n_sims, min = 152.4, max = 230.0)
-weight <- runif(n_sims, min = 20, max = 250)
-creat <- runif(n_sims, min = 10, max = 350)
+height <- runif(n_sims, min = 152.4, max = 230.0) # cm
+age <- as.integer(runif(n_sims, min = 18, max = 140))
+weight <- runif(n_sims, min = 20, max = 250) # kg
+creat <- runif(n_sims, min = 10, max = 350) # micromol/Litre
 
-sim_popn <- tibble(id = 1:n_sims, sex, age, height, weight, creat)
+sim_popn <- tibble(id = 1:n_sims, sex, height, age, weight, creat)
+
+sim_popn %>%
+  write_csv("data/sim_popn.csv")
