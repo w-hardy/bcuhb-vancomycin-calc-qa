2344bf6e8f7d6d105f5eaa2510816a3d2b7c7b69
w-hardy <will_hardy@icloud.com>
2022-03-19 19:23:56 +0000

    Added outputs from BCUHB chart, missing data check, and save results

--- r_scripts/r_dosing_calc.R
+++ r_scripts/r_dosing_calc.R
@@ -1,26 +1,72 @@
 #### Packages ####
 library(tidyverse)
+library(lubridate)
+library(hms)
 
 #### Data ####
 sim_popn <- read_csv("data/sim_popn.csv")
 
 #### Dosing ####
-sim_popn %>%
-  mutate(ideal_weight =
-           # Pai2000 - which versions should I be using? People under 5' (152.4cm)?
-           # 2.54 to convert cm to inches
-           case_when(sex == "Male" ~ 50 + 2.3 * (height / 2.54 - 60),
-                     sex == "Female" ~ 45.5 + 2.3 * (height / 2.54 - 60)),
-         dosing_weight = if_else(weight < ideal_weight, weight,
-                                 ideal_weight + (0.4 * (weight - ideal_weight))),
-         # What is the correct formula and conversion for eGFR?
-         # Cockcroft1976 0.0113 to convert from micromol/Litre to mg/100ml?
-         egfr = (140 - age) * weight / 72 * 0.0113 * creat,
-         egfr = case_when(sex == "Male" ~ egfr,
-                          sex == "Female" ~ 0.85 * egfr),
-         # Dosing information take from Thomson et al. (2009) - https://academic.oup.com/jac/article/63/5/1050/715674
-         inital_dose_min = weight * 15,
-         initial_dose_max = weight * 20,
-         maintainance_dose_min = case_when(between(egfr, 20, 49) ~ 15 * weight),
-         maintainance_dose_max = case_when(between(egfr, 20, 49) ~ 20 * weight)
-         )
+(r_results <-
+  sim_popn %>%
+     mutate(load_dose = case_when(weight < 60 ~ "1000mg",
+                                  weight >=60 & weight <=90 ~ "1500mg",
+                                  weight >90 ~ "2000mg"),
+            wght_gen_cnst = case_when(sex == "Female" ~ 45.5,
+                                      sex == "Male" ~ 50.0),
+            ibw = wght_gen_cnst + .91*(height-152.4),
+            ddw = ibw + (0.4*(weight - ibw)),
+            calc_weight = if_else(weight > 1.2*ibw, true = ibw, false = weight),
+            ren_gen_cnst = case_when(sex == "Female" ~ 1.04,
+                                     sex == "Male" ~ 1.23),
+            crcl = ((140-age)*ren_gen_cnst*calc_weight)/creat,
+            # Unclear which dose should be given for some pts (e.g., crcl == 89.5)
+            dose1 = case_when(crcl > 110 ~ "1500mg",
+                              crcl >= 90 & crcl <= 110 ~ "1250mg",
+                              crcl >= 75 & crcl < 90 ~ "1250mg",
+                              crcl >= 55 & crcl < 75 ~ "1250mg",
+                              crcl >= 40 & crcl < 55 ~ "1250mg",
+                              crcl >= 30 & crcl < 40 ~ "1250mg",
+                              crcl >= 20 & crcl < 30 ~ "1250mg",
+                              crcl < 20 ~ "500mg"),
+            dose2 = dose1,
+            dose3 = dose2,
+            dose4 = dose3,
+            dose_interval = case_when(crcl > 110 ~ hours(12),
+                                      crcl >= 90 & crcl <= 110 ~ hours(12),
+                                      crcl >= 75 & crcl < 90 ~ hours(12),
+                                      crcl >= 55 & crcl < 75 ~ hours(12),
+                                      crcl >= 40 & crcl < 55 ~ hours(12),
+                                      crcl >= 30 & crcl < 40 ~ hours(24),
+                                      crcl >= 20 & crcl < 30 ~ hours(24),
+                                      crcl < 20 ~ hours(48)),
+            dose1_time = datetime + dose_interval,
+            dose2_time = dose1_time + dose_interval,
+            dose3_time = dose2_time + dose_interval,
+            dose4_time = dose2_time + dose_interval,
+            # Assuming that level before 3rd dose should show at dose2
+            pre_level = case_when(crcl >= 40 ~ 2,
+                                  crcl < 40 ~ 3),
+            pre_level_time =
+              case_when(pre_level == 2 ~ as_hms(dose2_time),
+                        pre_level == 3 ~ as_hms(dose3_time)),
+            # What are working hours? Assumed 0700-1800
+            pre_level_work_time =
+              if_else(pre_level_time > hms(59, 59, 6) &
+                        pre_level_time < hms(00,00,18), TRUE, FALSE),
+            # If the level is needed before the xth dose and the xth dose is outside work hours, then bring it forward by 1
+            pre_level_before =
+              ifelse(pre_level_work_time == TRUE, pre_level, pre_level - 1)
+     ))
+
+
+### Check for missing values ###
+r_results %>%
+  summarise(across(everything(), ~sum(is.na(.)))) %>%
+  rowSums()
+
+
+#### Save results ####
+r_results %>%
+  write_csv("data/r_results.csv")
+
