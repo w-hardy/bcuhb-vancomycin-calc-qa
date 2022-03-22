806562017544070884d8e7fee347c78d66af5193
w-hardy <will_hardy@icloud.com>
2022-03-22 16:57:49 +0000

    Typos and comments

--- r_scripts/r_dosing_calc.R
+++ r_scripts/r_dosing_calc.R
@@ -16,18 +16,18 @@ sim_popn <- read_csv("data/sim_popn.csv")
                                       sex == "Male" ~ 50.0),
             ibw = wght_gen_cnst + .91*(height-152.4),
             ddw = ibw + (0.4*(weight - ibw)),
-            calc_weight = if_else(weight > 1.2*ibw, true = ibw, false = weight),
+            calc_weight = if_else(weight > 1.2*ibw, true = ddw, false = weight),
             ren_gen_cnst = case_when(sex == "Female" ~ 1.04,
                                      sex == "Male" ~ 1.23),
             crcl = ((140-age)*ren_gen_cnst*calc_weight)/creat,
             # Unclear which dose should be given for some pts (e.g., crcl == 89.5)
             dose1 = case_when(crcl > 110 ~ "1500mg",
                               crcl >= 90 & crcl <= 110 ~ "1250mg",
-                              crcl >= 75 & crcl < 90 ~ "1250mg",
-                              crcl >= 55 & crcl < 75 ~ "1250mg",
-                              crcl >= 40 & crcl < 55 ~ "1250mg",
-                              crcl >= 30 & crcl < 40 ~ "1250mg",
-                              crcl >= 20 & crcl < 30 ~ "1250mg",
+                              crcl >= 75 & crcl < 90 ~ "1000mg",
+                              crcl >= 55 & crcl < 75 ~ "750mg",
+                              crcl >= 40 & crcl < 55 ~ "500mg",
+                              crcl >= 30 & crcl < 40 ~ "750mg",
+                              crcl >= 20 & crcl < 30 ~ "500mg",
                               crcl < 20 ~ "500mg"),
             dose2 = dose1,
             dose3 = dose2,
@@ -43,20 +43,29 @@ sim_popn <- read_csv("data/sim_popn.csv")
             dose1_time = datetime + dose_interval,
             dose2_time = dose1_time + dose_interval,
             dose3_time = dose2_time + dose_interval,
-            dose4_time = dose2_time + dose_interval,
-            # Assuming that level before 3rd dose should show at dose2
-            pre_level = case_when(crcl >= 40 ~ 2,
-                                  crcl < 40 ~ 3),
+            dose4_time = dose3_time + dose_interval,
+            # Assuming that level before 3rd dose should show at dose3
+            pre_level = case_when(crcl >= 40 ~ 3,
+                                  crcl < 40 ~ 2),
+            # With no modification, when would the pre-dose level be scheduled?
             pre_level_time =
-              case_when(pre_level == 2 ~ as_hms(dose2_time),
-                        pre_level == 3 ~ as_hms(dose3_time)),
-            # What are working hours? Assumed 0700-1800
+              case_when(pre_level == 3 ~ as_hms(dose3_time),
+                        pre_level == 2 ~ as_hms(dose2_time)),
+            # What are working hours? Assumed 0600-1800
             pre_level_work_time =
-              if_else(pre_level_time > hms(59, 59, 6) &
+              if_else(pre_level_time > hms(59, 59, 5) &
                         pre_level_time < hms(00,00,18), TRUE, FALSE),
-            # If the level is needed before the xth dose and the xth dose is outside work hours, then bring it forward by 1
+            # For levels 1-5, on the dosing ladder the pre-dose level should
+             # ideally be done in working hours. Given the default is to take it
+             # at the 3rd dose, if this is outside working hours then it should
+             # be moved to the 4th dose.
+            # For levels 6-8, the gap between doses is 24 or 48 hours. Unless
+             # this dose time is moved within working hours, and therefore,
+             # away from the protocol, the pre-dose level will always be outside
+             # work hours, therefore should remain at 2
             pre_level_before =
-              ifelse(pre_level_work_time == TRUE, pre_level, pre_level - 1)
+              ifelse(dose_interval == period(12, "hours") & pre_level_work_time == FALSE,
+                     pre_level + 1, pre_level)
      ))
 
 
